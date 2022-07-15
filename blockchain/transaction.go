56ec4573d9c6c3e6c6308f8a764a8088a5534792
Dedalum <Dedalum@>
2022-07-15 16:43:14 +0200

    Reorganise code for transaction inputs and outputs

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -17,27 +17,6 @@ type Transaction struct {
 	Outputs []TxOutput
 }
 
-type TxOutput struct {
-	Value int
-	//Value would be representative of the amount of coins in a transaction
-	PubKey string
-	//The Pubkey is needed to "unlock" any coins within an Output. This indicated that YOU are the one that sent it.
-	//You are indentifiable by your PubKey
-	//PubKey in this iteration will be very straightforward, however in an actual application this is a more complex algorithm
-}
-
-//TxInput is represntative of a reference to a previous TxOutput
-type TxInput struct {
-	ID []byte
-	//ID will find the Transaction that a specific output is inside of
-	Out int
-	//Out will be the index of the specific output we found within a transaction.
-	//For example if a transaction has 4 outputs, we can use this "out" field to specify which output we are looking for
-	Sig string
-	//This would be a script that adds data to an outputs' PubKey
-	//however for this tutorial the Sig will be indentical to the PubKey.
-}
-
 func CoinbaseTx(toAddress, data string) *Transaction {
 	if data == "" {
 		data = fmt.Sprintf("Coins to %s", toAddress)
@@ -64,13 +43,6 @@ func (tx *Transaction) SetID() {
 	hash = sha256.Sum256(encoded.Bytes())
 	tx.Id = hash[:]
 
-}
-
-func (in *TxInput) CanUnlock(data string) bool {
-	return in.Sig == data
-}
-func (out *TxOutput) CanBeUnlocked(data string) bool {
-	return out.PubKey == data
 }
 
 func (tx *Transaction) IsCoinbase() bool {
